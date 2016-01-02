203e9d06fa69dedc1b5d87b5abf4755fa1569224
edreispi <edreispi@purdue.edu>
2016-01-01 19:09:22 -0500

    added level 7

---
+++ 7.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+
+# initial ssh command:
+spawn ssh bandit7@bandit.labs.overthewire.org
+
+# matches password after initial message:
+expect "*?password:*"
+
+# sends password recovered from last level
+send -- "HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs\r"
+
+# regular expression to check for prompt
+expect -re {\$ $}
+
+# solution:
+# send "ls\r"
+
+expect -re {\$ $}
+send "exit\r"
